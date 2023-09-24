6c2c43610a25fd57cf91d1b1bbc6f8a8d1fdec6a
lonerry <136971872+lonerry@users.noreply.github.com>
2023-09-24 16:10:54 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const board = document.querySelector('#board')
+const colors = ['#15F4EE','#39FF14','#BF00FF','#FFF01F','#FE59C2','#CCCCCC','#FF1818','#FF760D','#3A68E8']
+const SquaresNumbers = 1200
+for (let i = 0; i < SquaresNumbers;i++){
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover',()=>{
+        setColor(square)
+    })
+
+    square.addEventListener('mouseleave',()=>{
+        removeColor(square)
+    })
+
+
+    board.append(square)
+
+}
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #1d1d1d`
+}
+function getRandomColor() {
+   const index =  Math.floor(Math.random()*colors.length)
+   return colors[index]
+
+}
