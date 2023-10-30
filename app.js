17f5e82f1b55a82c80c12d09befc0cd8bedd582e
lonerry <136971872+lonerry@users.noreply.github.com>
2023-10-30 22:43:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,69 @@
-const board = document.querySelector('#board')
-const colors = ['#15F4EE','#39FF14','#BF00FF','#FFF01F','#FE59C2','#CCCCCC','#FF1818','#FF760D','#3A68E8']
-const SquaresNumbers = 1200
-for (let i = 0; i < SquaresNumbers;i++){
-    const square = document.createElement('div')
-    square.classList.add('square')
+const board = document.querySelector('#board');
+const colors = ['#15F4EE', '#39FF14', '#BF00FF', '#FFF01F', '#FE59C2', '#CCCCCC', '#FF1818', '#FF760D', '#3A68E8'];
+const SquaresNumbers = 1200;
 
-   
-    square.addEventListener('touchstart',()=>{
-        setColor(square)
-    })
+// Создаем массив квадратов
+const squares = [];
 
-  
-    square.addEventListener('touchend',()=>{
-        removeColor(square)
-    })
-    
+for (let i = 0; i < SquaresNumbers; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  squares.push(square);
 
+  square.addEventListener('mouseenter', () => {
+    highlightSquare(square);
+  });
 
-    board.append(square)
+  square.addEventListener('mouseleave', () => {
+    resetSquare(square);
+  });
 
+  board.append(square);
 }
-function setColor(element) {
-    const color = getRandomColor()
-    element.style.backgroundColor = color
-    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+
+const mc = new Hammer.Manager(board);
+
+mc.add(new Hammer.Tap());
+const pan = new Hammer.Pan({ direction: Hammer.DIRECTION_ALL });
+pan.set({ threshold: 5 }); // Установите порог на ваш вкус
+mc.add(pan);
+
+let currentSquare = null;
+
+mc.on('tap', (event) => {
+  const target = event.target;
+  if (target.classList.contains('square')) {
+    highlightSquare(target);
+  }
+});
+
+mc.on('panstart panmove', (event) => {
+  const target = document.elementFromPoint(event.center.x, event.center.y);
+  if (target.classList.contains('square')) {
+    highlightSquare(target);
+  } else {
+    squares.forEach(resetSquare);
+  }
+});
+
+mc.on('panend', () => {
+  squares.forEach(resetSquare);
+});
+
+function highlightSquare(square) {
+  const color = getRandomColor();
+  square.style.backgroundColor = color;
+  square.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
-function removeColor(element) {
-    element.style.backgroundColor = '#1d1d1d'
-    element.style.boxShadow = `0 0 2px #1d1d1d`
+
+function resetSquare(square) {
+  square.style.backgroundColor = '#1d1d1d';
+  square.style.boxShadow = '0 0 2px #1d1d1d';
 }
+
 function getRandomColor() {
-   const index =  Math.floor(Math.random()*colors.length)
-   return colors[index]
-
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
 }
 
 
